f9886b02bfbce02f513e60903364568695a8349b
AlexCrux0407 <130202688+AlexCrux0407@users.noreply.github.com>
2024-11-30 20:40:24 -0600

    Update web.php

--- proyectoPW/routes/web.php
+++ proyectoPW/routes/web.php
@@ -73,10 +73,12 @@ Route::delete('/admin/hoteles/{id}', [AdminController::class, 'deleteHotel'])->n
 
 
 //rutas reservacion de servicios
-Route::view('/reservacion','reservacion')->name('reservacion');;
+Route::view('/reservacion','reservacion');
+Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create'); 
 Route::post('/reservations/store', [ReservationController::class, 'store'])->name('reservations.store'); 
 Route::get('/reservations/reservacion', [ReservationController::class, 'reservacion'])->name('reservations.reservacion'); 
-Route::get('/reservations/user', [ReservationController::class, 'userReservations'])->name('reservations.user');
+Route::get('/reservations/user', [ReservationController::class, 'userReservations'])->name('reservations.user'); 
+Route::get('/reservations/cancel/{id}', [ReservationController::class, 'cancelReservation'])->name('reservations.cancel');
 
 // Rutas para ajustar tarifas
 Route::post('/admin/vuelos/ajustar-tarifas', [AdminController::class, 'ajustarTarifasVuelo'])->name('admin.ajustarTarifasVuelo');
